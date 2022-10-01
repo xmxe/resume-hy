920c6f6b43ee3baa091fc236824067ee78a526e1
xmxe <464817304@qq.com>
2022-10-01 19:58:02 +0800

    去除<a>的边框线&暂停/继续换成svg

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,7 +1,11 @@
 <template>
   <footer>
-    <a href="#" v-show="!isEnd" @click.prevent="togglePause">{{ text }}</a>
-    <a href="#" v-show="!isEnd" @click.prevent="skip">跳过-></a>
+    <a href="#" v-show="!isEnd" @click.prevent="togglePause" v-html="text"></a>
+    <a href="#" v-show="!isEnd" @click.prevent="skip">
+      <svg width="22" height="22" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M4 40.8361C8.89307 34.8632 13.2383 31.4739 17.0356 30.6682C20.8329 29.8625 24.4483 29.7408 27.8818 30.303V41L44 23.5453L27.8818 7V17.167C21.5333 17.2172 16.1362 19.4948 11.6905 24C7.24474 28.5052 4.68126 34.1172 4 40.8361Z" fill="#000000" stroke="#000000" stroke-width="3" stroke-linejoin="round"/>
+      </svg>
+    </a>
     <a href="https://github.com/xmxe/" v-show="isEnd">
       <svg width="23" height="28" class="icon" viewBox="0 0 16 14" version="1.1" aria-hidden="true">
         <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
@@ -27,7 +31,11 @@
 <script>
   const STATE_RUNNING = 0
   const STATE_PAUSED = 1
-  const pauseText = ['暂停<<', '继续>>']
+  // const pauseText = ['暂停<<', '继续>>']
+  const pauseText = [
+    '<?xml version="1.0" encoding="UTF-8"?><svg width="28" height="28" viewBox="0 0 48 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 12V36" stroke="#000000" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/><path d="M32 12V36" stroke="#000000" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    '<?xml version="1.0" encoding="UTF-8"?><svg width="28" height="28" viewBox="0 0 48 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 24V11.8756L25.5 17.9378L36 24L25.5 30.0622L15 36.1244V24Z" fill="#000000" stroke="#000000" stroke-width="3" stroke-linejoin="round"/></svg>'
+    ]
 
   export default {
     name: 'v-footer',
